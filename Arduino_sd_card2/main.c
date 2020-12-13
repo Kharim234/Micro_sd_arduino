0fa498f1a76d09c0ea1a7f81101852f7658d3425
Michal <malinowskimichal27@gmail.com>
2020-12-13 20:16:14 +0100

    comment sd card example code, change timer to achieve stable 1s, add example temperature measurement

--- Arduino_sd_card2/main.c
+++ Arduino_sd_card2/main.c
@@ -7,6 +7,7 @@
 #include "ff.h"		/* Declarations of FatFs API */
 #include <stdlib.h>
 #include <avr/interrupt.h>
+#include <string.h>
 
 FATFS FatFs;		/* FatFs work area needed for each volume */
 FIL Fil;			/* File object needed for each open file */
@@ -17,8 +18,10 @@ FIL Fil;			/* File object needed for each open file */
 #define SET_LED_OFF() (PORTB &= ~LED_PIN)
 
 volatile uint16_t test = 0;
-
-uint16_t licznik;
+volatile uint32_t licznik_32bit = 0;
+volatile uint16_t licznik;
+volatile uint8_t flag= 0;
+volatile uint16_t adc_result;
 
 void USART_Init( unsigned int ubrr)
 {
@@ -105,7 +108,7 @@ TIMSK0 = (1<<OCIE0A); //  Timer/Counter Output Compare Match B Interrupt Enable
 // Time [s] = {2 x Prescaler x ( 1 + OCR0A )} / F_CPU (16 000 000)
 	
 //OCR0A = {( Time x F_CPU ) / ( 2 x Prescaler )} - 1
-OCR0A = 156; //156 -> 20,1ms
+OCR0A = 124; //156 -> 10,05ms, 124 -> 8ms
 sei();
 
 }
@@ -159,24 +162,25 @@ void start_conversion_asynchro (void){
 	//uart_puts_rn(b);
 }
 
+
+
 ISR(ADC_vect)
 {
-	uint16_t result;
-	result = conversion_result();
+	
+	adc_result = conversion_result();
 		
 	//ADCSRA |= (1<<ADIF);
+	flag = 1;
 		
-	char b[ 32 ];
-	itoa( result, b, 10 );
-	uart_puts("ADC asynchro ");
-	uart_puts_rn(b);
+
 	
 }
 ISR(TIMER0_COMPA_vect)
 {
 	// user code here
 	licznik++;
-	if(licznik > 100){
+	licznik_32bit++;
+	if(licznik >= 125){ //dopelnienie do 1sec
 		//uart_puts("IT works");
 		licznik = 0;
 		start_conversion_asynchro();
@@ -190,7 +194,7 @@ int main (void) // clock 16 Mhz
 
 	//CLKPR  = 1<<CLKPCE; //change clock to 8 Mhz
 	//CLKPR  = 1<<CLKPS0;
-	USART_Init_Baud_Rate( 9600 ); // do not work at very high baud rate
+	USART_Init_Baud_Rate( 38400 ); // do not work at very high baud rate
 	//USART_Init(1); //Use this when you want to get very very high baud rate
 	
 	
@@ -198,7 +202,7 @@ int main (void) // clock 16 Mhz
 	adc_init();
 	//start_conversion_synchro();
 	//start_conversion_synchro();
-	uart_puts_rn("Arduino Booted2");
+	//uart_puts_rn("Arduino Booted2");
 	//start_conversion_asynchro();
 // 	while(1){
 // 		_delay_ms(500);
@@ -222,79 +226,124 @@ init_timer();
 
 //
 UINT licznik = 0;
-do 
-{
-	fr = f_open(&Fil, "WRITE.TXT", FA_WRITE | FA_CREATE_ALWAYS );	/* Create a file */
-	Sent_error_message(fr, "File open WRITE.TXT");
-	licznik ++;
-	_delay_ms(50);
-} while (fr!=FR_OK&& licznik < 10);
+// do 
+// {
+// 	fr = f_open(&Fil, "WRITE.TXT", FA_WRITE | FA_CREATE_ALWAYS );	/* Create a file */
+// 	Sent_error_message(fr, "File open WRITE.TXT");
+// 	licznik ++;
+// 	_delay_ms(50);
+// } while (fr!=FR_OK&& licznik < 10);
+// 
+// if (fr == FR_OK) {
+// 	UINT Bytes_Written;
+// 	BYTE Bytes_to_write[512];
+// 	for(int i = 0; i < 512;i++)
+// 		Bytes_to_write[i] = i;
+// 	for(int i = 0; i < 100;i++){
+// 		fr = f_write(&Fil, Bytes_to_write, 512, &Bytes_Written);	/* Write data to the file */
+// 		Sent_error_message(fr, "File write WRITE.TXT");
+// 	}
+// 	fr = f_close(&Fil);
+// 	Sent_error_message(fr, "Close file WRITE.TXT");
+// }
+// 
+// 
+// 
+// 	fr = f_open(&Fil, "PANTAD.TXT", FA_WRITE | FA_OPEN_APPEND | FA_READ);	/* Create a file */
+// 	Sent_error_message(fr, "File open PANTAD.TXT");
+// 	if (fr == FR_OK) {
+// 		//SET_LED_ON();
+// 		
+// 		//UINT Bytes_Written;
+// 	//	fr = f_write(&Fil, "It works!\r\n", 11, &Bytes_Written);	/* Write data to the file */
+// 								/* Close the file */
+// 		//if (fr == FR_OK && Bytes_Written == 11) {		/* Lights green LED if data written well */
+// 		//	uart_puts_rn("Write to file succesfully");
+// 			//DDRB |= (1<<5); PORTB |= (1<<5);	/* Set PB4 high */
+// 		//}
+// 		
+// 		fr = f_rewind(&Fil);
+// 		Sent_error_message(fr, "Rewind file");
+// 		/*
+// 		UINT Bytes_to_read = 512;
+// 		UINT Bytes_readed = 0;
+// 		do 
+// 		{
+// 			
+// 			BYTE Buff[520];
+// 
+// 			fr = f_read ( &Fil, Buff, Bytes_to_read, &Bytes_readed);
+// 			//Sent_error_message(fr, "Read file");
+// 			if(fr) Sent_error_message(fr, "Read file");
+// 			
+// 			if (fr == FR_OK){
+// 				//uart_puts("Number of readed bytes: ");
+// 				char b[ 32 ];
+// 				itoa( Bytes_readed, b, 10 );
+// 				//uart_puts_rn(b);
+// 				
+// 				//uart_puts_rn("Bytes Readed: ");
+// 				uart_puts_with_length((char*)(Buff), Bytes_readed);
+// 				//uart_puts_rn("");
+// 				if(Bytes_to_read != Bytes_readed){
+// 					uart_puts_rn("End of file");
+// 				}
+// 			}
+// 			
+// 		} while (Bytes_to_read == Bytes_readed && fr == FR_OK);
+// 		*/
+// 		
+// 		fr = f_close(&Fil);	
+// 		Sent_error_message(fr, "Close file");
+// 	}
 
-if (fr == FR_OK) {
-	UINT Bytes_Written;
-	BYTE Bytes_to_write[512];
-	for(int i = 0; i < 512;i++)
-		Bytes_to_write[i] = i;
-	for(int i = 0; i < 100;i++){
-		fr = f_write(&Fil, Bytes_to_write, 512, &Bytes_Written);	/* Write data to the file */
-		Sent_error_message(fr, "File write WRITE.TXT");
-	}
-	fr = f_close(&Fil);
-	Sent_error_message(fr, "Close file WRITE.TXT");
-}
-
-
-
-	fr = f_open(&Fil, "PANTAD.TXT", FA_WRITE | FA_OPEN_APPEND | FA_READ);	/* Create a file */
-	Sent_error_message(fr, "File open PANTAD.TXT");
-	if (fr == FR_OK) {
-		//SET_LED_ON();
-		
-		//UINT Bytes_Written;
-	//	fr = f_write(&Fil, "It works!\r\n", 11, &Bytes_Written);	/* Write data to the file */
-								/* Close the file */
-		//if (fr == FR_OK && Bytes_Written == 11) {		/* Lights green LED if data written well */
-		//	uart_puts_rn("Write to file succesfully");
-			//DDRB |= (1<<5); PORTB |= (1<<5);	/* Set PB4 high */
-		//}
-		
-		fr = f_rewind(&Fil);
-		Sent_error_message(fr, "Rewind file");
-		
-		UINT Bytes_to_read = 512;
-		UINT Bytes_readed = 0;
-		do 
-		{
-			
-			BYTE Buff[520];
-
-			fr = f_read ( &Fil, Buff, Bytes_to_read, &Bytes_readed);
-			//Sent_error_message(fr, "Read file");
-			if(fr) Sent_error_message(fr, "Read file");
-			
-			if (fr == FR_OK){
-				//uart_puts("Number of readed bytes: ");
-				char b[ 32 ];
-				itoa( Bytes_readed, b, 10 );
-				//uart_puts_rn(b);
+// 	for (;;) {
+// 
+// 	}
+	while(1){
+		if(flag){
+				flag = 0;
+				char adc_result_string[ 32 ];
+				char licznik_32bit_string[ 32 ];
+				char string_size[ 32 ];
+				utoa( adc_result, adc_result_string, 10 );
+				utoa( licznik_32bit, licznik_32bit_string, 10 );
+				UINT string_size_int = strlen(licznik_32bit_string);
+				utoa( string_size_int, string_size, 10 );
 				
-				//uart_puts_rn("Bytes Readed: ");
-				uart_puts_with_length((char*)(Buff), Bytes_readed);
-				//uart_puts_rn("");
-				if(Bytes_to_read != Bytes_readed){
-					uart_puts_rn("End of file");
-				}
-			}
-			
-		} while (Bytes_to_read == Bytes_readed && fr == FR_OK);
+				uart_puts("String size: ");
+				uart_puts(string_size);
+				uart_puts(" Licznik_32bit: ");
+				uart_puts(licznik_32bit_string);
+				uart_puts(" ADC asynchro ");
+				uart_puts_rn(adc_result_string);
+				char string_to_sd[64];
+				string_to_sd[0]= '\0';
+				
+				strcpy(string_to_sd, "String size: ");
+				char *p = string_to_sd + strlen("String size: ");
+				strcpy(p, licznik_32bit_string);
+				uart_puts_rn(string_to_sd);
+				
+				
+// 				fr = f_open(&Fil, "WRITE.TXT", FA_WRITE | FA_CREATE_ALWAYS );	/* Create a file */
+// 				Sent_error_message(fr, "File open WRITE.TXT");
+// 				if (fr == FR_OK) {
+// 					UINT Bytes_Written;
+// 					BYTE Bytes_to_write[512];
+// 					for(int i = 0; i < 512;i++)
+// 					Bytes_to_write[i] = i;
+// 					for(int i = 0; i < 100;i++){
+// 						fr = f_write(&Fil, Bytes_to_write, 512, &Bytes_Written);	/* Write data to the file */
+// 						Sent_error_message(fr, "File write WRITE.TXT");
+// 					}
+// 					fr = f_close(&Fil);
+// 					Sent_error_message(fr, "Close file WRITE.TXT");
+// 				
+// 				}
 		
 		
-		fr = f_close(&Fil);	
-		Sent_error_message(fr, "Close file");
-	}
-
-	for (;;) {
-
+		}
 	}
 }
 
